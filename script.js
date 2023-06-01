6d4d7151b29ab4170a505e159eef6bc8a33b86a3
Akshaybaisware <130745994+Akshaybaisware@users.noreply.github.com>
2023-06-01 13:43:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+window.onload =function()
+	{
+		let line = document.getElementById("line");
+		let angle = 0;
+		let speed = 1;
+		function rotateLine(){
+			line.style.transform="rotate(" + angle + "deg)";
+			angle+=speed;
+			requestAnimationFrame(rotateLine);
+		}
+		rotateLine();
+	}
